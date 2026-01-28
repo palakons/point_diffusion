feat: enhance training evaluation with GPU logging and fix loss calculation

adaln0_ddpm.py
@@ -23,7 +23,13 @@ import os
 import argparse
 from torch.utils.data import Subset
 import torch.nn.functional as F
-from dit_ddpm_class import parse_args, set_seed, TransformerDenoiser, get_runid
+from dit_ddpm_class import (
+    parse_args,
+    set_seed,
+    TransformerDenoiser,
+    get_runid,
+    query_gpu_stats,
+)
 from torch.utils.tensorboard import SummaryWriter
 from adaln0_import import (
     save_checkpoint,
@@ -156,6 +162,18 @@ def train_vae_voxel_ddpm(
             writer.add_scalar("Loss/val/bce", avg_val_bce_loss, epoch + 1)
             writer.add_scalar("Loss/val/total", avg_val_total_loss, epoch + 1)
 
+        if (epoch + 1) % config["gpu_log_every"] == 0:
+            stats = query_gpu_stats(gpu_index=0)
+            if stats is not None:
+                writer.add_scalar("gpu/utilization_pct", stats["util_gpu"], epoch + 1)
+                writer.add_scalar("gpu/power_w", stats["power_w"], epoch + 1)
+                writer.add_scalar(
+                    "gpu/memory_used_mib", stats["mem_used_mib"], epoch + 1
+                )
+                writer.add_scalar(
+                    "gpu/memory_total_mib", stats["mem_total_mib"], epoch + 1
+                )
+
         if (epoch + 1) % config[
             "save_every"
         ] == 0 or avg_total_train_loss < best_epoch_train_total_loss:
@@ -198,7 +216,7 @@ def train_vae_voxel_ddpm(
 
     tblogHparam(config, writer, {"avg_ddpm_train_loss": avg_ddpm_train_loss})
     writer.close()
-    checkpoint_path = path.join(checkpoint_dir, f"final_vae_voxel_ddpm_{run_id}.pth")
+    checkpoint_path = os.path.join(checkpoint_dir, f"final_vae_voxel_ddpm_{run_id}.pth")
     save_checkpoint(
         ddpm,
         optimizer,
@@ -206,7 +224,7 @@ def train_vae_voxel_ddpm(
         {
             "ddpm_loss": avg_ddpm_train_loss,
         },
-        os.checkpoint_path,
+        checkpoint_path,
         config,
     )
     print(f"Training completed. Final model saved.", checkpoint_path)

adaln0_import.py
@@ -322,7 +322,7 @@ def train_eval_batch(
             ddpm_loss = loss_fn(noise, noise_pred)
             sum_ddpm_loss += ddpm_loss.item()
             total_loss = ddpm_loss
-            bce_loss = None
+            bce_loss = 0
 
             if bce_weight > 0:
                 # Estimate x0 from current x_t and eps_pred
@@ -333,7 +333,11 @@ def train_eval_batch(
                 # BCE targets in {0,1} (since your x0 is {-1,+1})
                 logits = bce_logit_scale * x0_pred
                 bce_loss = F.binary_cross_entropy_with_logits(
-                    logits, (center_occupancy_grid > 0).float()
+                    logits,
+                    (
+                        center_occupancy_grid
+                        > (0 if config["grid_binary_range"] == "neg1-1" else 0.5)
+                    ).float(),
                 )
 
                 total_loss = total_loss + bce_weight * bce_loss

man_ddpm.py
@@ -84,10 +84,10 @@ class SimplePerspectiveCamera:
         return uvz, camera_points
 
 
-def chamfer_distance(pred_uvz, gt_uvz):
+def chamfer_distance(pred_uvz, gt_uvz, device):
     # convert to tensor
-    pred_uvz = torch.tensor(pred_uvz)
-    gt_uvz = torch.tensor(gt_uvz)
+    pred_uvz = torch.tensor(pred_uvz, device=device)
+    gt_uvz = torch.tensor(gt_uvz, device=device)
     # print("shape of pred_uvz:", pred_uvz.shape, type(pred_uvz))
     # print("shape of gt_uvz:", gt_uvz.shape, type(gt_uvz))
     """
@@ -152,6 +152,7 @@ class MANDataset(Dataset):
         viz_dir: str = "",
         grid_binary_range: str = "0-1",  # "0-1" or "neg1-1"
         keep_frames=0,
+        get_bb=False,  # whether to get bounding box of radar points
     ):  # load all frames from scene_id, of data_file, particular radar and camera channel
         self.device = device
         self.data_file = data_file
@@ -178,6 +179,7 @@ class MANDataset(Dataset):
         self.viz_dir = viz_dir
         self.grid_binary_range = grid_binary_range
         self.keep_frames = keep_frames
+        self.get_bb = get_bb
 
         if self.wan_vae:
             print(f"Loading VAE...")
@@ -233,7 +235,9 @@ class MANDataset(Dataset):
             ):
                 self.data_bank.append(
                     {
-                        **self.load_data(trucksc, frame_token, self.point_only),
+                        **self.load_data(
+                            trucksc, frame_token, self.point_only, self.get_bb
+                        ),
                         "scene_id": scene_id,
                         "frame_index": pbar.n,
                     }
@@ -863,7 +867,31 @@ class MANDataset(Dataset):
                 clip_path = batch_feature_files[j]
                 torch.save(tokens[j].cpu(), clip_path)  # ([ 257, 1024])
 
-    def load_data(self, trucksc, frame_token, point_only=False):
+    def _get_cam_bboxes_for_sample(
+        self, trucksc, sample, cam_token: str, verbose: bool = False
+    ):
+        """
+        Return a list of (ann_token, bbox) for annotations that belong to the given camera sample_data token.
+
+        Note:
+        MAN/nuScenes annotations live at the sample-level; some datasets include a
+        'sensor_data_token' on the annotation to indicate which sensor it was created for.
+        """
+        out = []
+        ann_tokens = sample["anns"]
+        for ann_token in ann_tokens:
+            ann = trucksc.get("sample_annotation", ann_token)
+
+            if verbose:
+                print(f"ann_token: {ann_token}")
+                for k, v in ann.items():
+                    print(f"  {k}: {v}")
+
+            # trucksc.get_box expects an annotation token
+            out.append((ann_token, trucksc.get_box(ann_token)))
+        return out
+
+    def load_data(self, trucksc, frame_token, point_only=False, get_bb=False):
         time_0 = time.time()
 
         da3_model = None
@@ -871,6 +899,22 @@ class MANDataset(Dataset):
         frame = trucksc.get("sample", frame_token)
         cam = trucksc.get("sample_data", frame["data"][self.camera_channel])
         radar = trucksc.get("sample_data", frame["data"][self.radar_channel])
+
+        # -1) get annotated Bouding Box of this nuScene-like frame
+        if get_bb:
+            cam_token = cam["token"]
+            boxes = self._get_cam_bboxes_for_sample(
+                trucksc, frame, cam_token, verbose=True
+            )  # frame IS my_sample
+
+            print(f"Frame token: {frame_token}")
+            print(f"Camera token: {cam_token}")
+            print(f"Found {len(boxes)} boxes")
+            for i, (ann_token, bbox) in enumerate(boxes):
+                ann = trucksc.get("sample_annotation", ann_token)
+                if ann["num_radar_pts"] > 0:
+                    print(i, ann)
+            exit()
 
         # 0) load CLIP
         time_1 = time.time()
@@ -1014,7 +1058,7 @@ class MANDataset(Dataset):
         points_uvz = image_coord[:, :3]  # N x3
         time_11 = time.time()
 
-        if self.wan_vae:
+        if self.wan_vae and not point_only:
             # infer
 
             w, h = 832, 480
@@ -1160,6 +1204,8 @@ class MANDataset(Dataset):
                 "npoints_filtered": npoints_filtered,
                 "clip_feature": clip_feature,
             }
+            if self.wan_vae:
+                output["wan_vae_latent"] = wan_vae_latent  # add wan vae latent
         else:
             output = {
                 "filtered_radar_data": output_filtered_radar_data,
@@ -1168,8 +1214,6 @@ class MANDataset(Dataset):
                 "npoints_original": npoints_original,
                 "npoints_filtered": npoints_filtered,
             }
-        if self.wan_vae:
-            output["wan_vae_latent"] = wan_vae_latent  # add wan vae latent
         return output
 
     def __len__(self):
@@ -1291,6 +1335,7 @@ class MANDataset(Dataset):
             "reconstructed": {"color": "red", "marker": "x", "size": 10, "alpha": 0.6},
         },
         fig_size=(20, 20),
+        device="cpu",
     ):
         """
         Visualize original vs reconstructed UVZ point clouds.
@@ -1328,6 +1373,7 @@ class MANDataset(Dataset):
             s=marker_config["original"]["size"],
             alpha=marker_config["original"]["alpha"],
             marker=marker_config["original"]["marker"],
+            rasterized=True,
         )
         ax1.scatter(
             recon_np[:, 0],
@@ -1337,14 +1383,17 @@ class MANDataset(Dataset):
             s=marker_config["reconstructed"]["size"],
             alpha=marker_config["reconstructed"]["alpha"],
             marker=marker_config["reconstructed"]["marker"],
+            rasterized=True,
         )
-        ax1.legend()
+        ax1.legend(loc="upper right")  # () is slow
         ax1.set_title(
-            f"UV View (n=ori{len(orig_np)}, recon{len(recon_np)}) CD:{chamfer_distance(orig_np[None,:,:] , recon_np[None,:,:]):.4f}"
+            f"UV View (n=ori{len(orig_np)}, recon{len(recon_np)}) CD:{chamfer_distance(orig_np[None,:,:] , recon_np[None,:,:],device):.4f}"
         )
         ax1.axis("equal")
         ax1.set_xlim(plotlims["u"])
         ax1.set_ylim(plotlims["v"])
+        ax1.set_xlabel("U-Horizontal Pixel")
+        ax1.set_ylabel("V-Vertical Pixel")
 
         ax2 = fig.add_subplot(2, 2, 2)
         ax2.scatter(
@@ -1355,6 +1404,7 @@ class MANDataset(Dataset):
             s=marker_config["original"]["size"],
             alpha=marker_config["original"]["alpha"],
             marker=marker_config["original"]["marker"],
+            rasterized=True,
         )
         ax2.scatter(
             recon_np[:, 0],
@@ -1364,63 +1414,75 @@ class MANDataset(Dataset):
             s=marker_config["reconstructed"]["size"],
             alpha=marker_config["reconstructed"]["alpha"],
             marker=marker_config["reconstructed"]["marker"],
+            rasterized=True,
         )
-        ax2.legend()
+        ax2.legend(loc="upper right")
         ax2.set_title("UZ View")
         ax2.set_xlim(plotlims["u"])
         ax2.set_ylim(plotlims["z"])
+        ax2.set_xlabel("U-Horizontal Pixel")
+        ax2.set_ylabel("Z-Depth (m)")
 
         ax3 = fig.add_subplot(2, 2, 3)
         ax3.scatter(
-            orig_np[:, 1],
             orig_np[:, 2],
+            orig_np[:, 1],
             color=marker_config["original"]["color"],
             label="Original",
             s=marker_config["original"]["size"],
             alpha=marker_config["original"]["alpha"],
             marker=marker_config["original"]["marker"],
+            rasterized=True,
         )
         ax3.scatter(
-            recon_np[:, 1],
             recon_np[:, 2],
+            recon_np[:, 1],
             color=marker_config["reconstructed"]["color"],
             label="Reconstructed",
             s=marker_config["reconstructed"]["size"],
             alpha=marker_config["reconstructed"]["alpha"],
             marker=marker_config["reconstructed"]["marker"],
+            rasterized=True,
         )
-        ax3.legend()
+        ax3.legend(loc="upper right")
         ax3.set_title("VZ View")
-        ax3.set_xlim(plotlims["v"])
-        ax3.set_ylim(plotlims["z"])
+        ax3.set_ylim(plotlims["v"])
+        ax3.set_xlim(plotlims["z"])
+        ax3.set_ylabel("V-Vertical Pixel")
+        ax3.set_xlabel("Z-Depth (m)")
 
         ax4 = fig.add_subplot(2, 2, 4, projection="3d")
         ax4.scatter(
             orig_np[:, 0],
-            orig_np[:, 1],
             orig_np[:, 2],
+            orig_np[:, 1],
             color=marker_config["original"]["color"],
             label="Original",
             s=marker_config["original"]["size"],
             alpha=marker_config["original"]["alpha"],
             marker=marker_config["original"]["marker"],
+            rasterized=True,
         )
         ax4.scatter(
             recon_np[:, 0],
-            recon_np[:, 1],
             recon_np[:, 2],
+            recon_np[:, 1],
             color=marker_config["reconstructed"]["color"],
             label="Reconstructed",
             s=marker_config["reconstructed"]["size"],
             alpha=marker_config["reconstructed"]["alpha"],
             marker=marker_config["reconstructed"]["marker"],
+            rasterized=True,
         )
-        ax4.legend()
+        ax4.legend(loc="upper right")
 
         ax4.set_title(f"3D View")
         ax4.set_xlim(plotlims["u"])
-        ax4.set_ylim(plotlims["v"])
-        ax4.set_zlim(plotlims["z"])
+        ax4.set_zlim(plotlims["v"])
+        ax4.set_ylim(plotlims["z"])
+        ax4.set_xlabel("U-Horizontal Pixel")
+        ax4.set_zlabel("V-Vertical Pixel")
+        ax4.set_ylabel("Z-Depth (m)")
 
         if title:
             # make sure wrapt he line to 80 characters

sample_adanl0_ddpm.py
@@ -19,7 +19,7 @@ from man_ddpm import MANDataset
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 from datetime import datetime
-import os
+import os, time
 import argparse
 from torch.utils.data import Subset
 import torch.nn.functional as F
@@ -179,10 +179,11 @@ def sample_vae_voxel_ddpm(
     for batch in tqdm(dataloader, desc="Sampling DDPM", leave=True):  # stays
         bs = batch["wan_vae_latent"].shape[0]
         frame_tokens = batch["frame_token"]
+        actual_occupancy_grid = batch["occupancy_grid"].to(config["device"])
         center_wan_vae_latent, center_actual_occupancy_grid = (
             get_center_crop_latent_and_grid(
                 batch["wan_vae_latent"].to(config["device"]),
-                batch["occupancy_grid"].to(config["device"]),
+                actual_occupancy_grid,
             )
         )
         batch_size = center_wan_vae_latent.shape[0]
@@ -194,17 +195,47 @@ def sample_vae_voxel_ddpm(
                 model=ddpm,
                 config=config,
             )
+            pad_left = (
+                config["scaled_image_size"][1] - center_actual_occupancy_grid.shape[3]
+            ) // 2
+            pad_right = (
+                config["scaled_image_size"][1]
+                - pad_left
+                - center_actual_occupancy_grid.shape[3]
+            )
+            # print("pad_left, pad_right:", pad_left, pad_right)
+
             # print(
             #     "shape of sampled occupancy grid:", sampled_occupancy_grid.shape
             # )  # [2,  128, 128, 256]
-            for time_step in tqdm(sampled_center_occupancy_grids, leave=False):
-                for i_batch in trange(
-                    bs, desc="Each iteM in batch", leave=False
-                ):  # leave=True (default behavior): The progress bar stays
-                    print("Time step:", time_step)
+            tstq = tqdm(
+                sorted(sampled_center_occupancy_grids.keys()),
+                leave=False,
+                desc="Time steps",
+            )
+            for time_step in tstq:  # this step takes time
+                time_0 = time.time()
+                padded_sampled_occupancy_grids = F.pad(
+                    sampled_center_occupancy_grids[time_step],
+                    (pad_left, pad_right),
+                    mode="constant",
+                    value=0,
+                )  # pad some dimensions of input are described starting from the last dimension
+                # print(
+                #     "sampled_center_occupancy_grids shape:",
+                #     sampled_center_occupancy_grids[time_step].shape,
+                # )
+                # print("padded shape:", padded_sampled_occupancy_grids.shape)
+                time_1 = time.time()
+                trr = trange(bs, desc="Each in batch", leave=False)
+                for (
+                    i_batch
+                ) in trr:  # leave=True (default behavior): The progress bar stays
+                    # print("Time step:", time_step)
+                    time_2 = time.time()
                     reconstructed_uvz = (
-                        dataloader.dataset.dataset.dataset.occupancy_grid_to_uvz(
-                            sampled_center_occupancy_grids[time_step][i_batch, :, :, :],
+                        dataloader.dataset.dataset.occupancy_grid_to_uvz(
+                            padded_sampled_occupancy_grids[i_batch, :, :, :],
                             original_image_size=config["original_image_size"],
                             max_depth=config["max_voxel_grid_depth"],
                             threshold=(
@@ -212,16 +243,16 @@ def sample_vae_voxel_ddpm(
                             ),
                         )
                     )
-                    actual_uvz = (
-                        dataloader.dataset.dataset.dataset.occupancy_grid_to_uvz(
-                            center_actual_occupancy_grid[i_batch, :, :, :],
-                            original_image_size=config["original_image_size"],
-                            max_depth=config["max_voxel_grid_depth"],
-                            threshold=(
-                                0.0 if config["grid_binary_range"] == "neg1-1" else 0.5
-                            ),
-                        )
+                    time_3 = time.time()
+                    actual_uvz = dataloader.dataset.dataset.occupancy_grid_to_uvz(
+                        actual_occupancy_grid[i_batch, :, :, :],
+                        original_image_size=config["original_image_size"],
+                        max_depth=config["max_voxel_grid_depth"],
+                        threshold=(
+                            0.0 if config["grid_binary_range"] == "neg1-1" else 0.5
+                        ),
                     )
+                    time_4 = time.time()
                     # print(
                     #     "min max of reconstructed uvz each dim:",
                     #     reconstructed_uvz.amin(dim=0),
@@ -235,8 +266,8 @@ def sample_vae_voxel_ddpm(
                     # print(
                     #     f'Chamfer Distance: {chamfer_distance(actual_uvz.unsqueeze(0).to(config["device"]),reconstructed_uvz.unsqueeze(0).to(config["device"]),).item():.4f}'
                     # )
-
-                    dataloader.dataset.dataset.dataset.visualize_uvz_comparison(
+                    time_5 = time.time()
+                    dataloader.dataset.dataset.visualize_uvz_comparison(
                         frame_token=frame_tokens[i_batch]
                         + f"_grid_{run_id}_step_{time_step:03d}",
                         title=frame_tokens[i_batch]
@@ -259,8 +290,23 @@ def sample_vae_voxel_ddpm(
                             },
                         },
                         fig_size=(16, 9),  # width, height in inches
+                        plotlims={
+                            "u": (0, config["original_image_size"][1]),
+                            "v": (0, config["original_image_size"][0]),
+                            "z": (0, config["max_voxel_grid_depth"]),
+                        },
+                        device=config["device"],
                     )
-                    # print("saved uvz comparison plot.", plot_dir)
+                    time_6 = time.time()
+                    trr.set_description(
+                        f"recon{time_3 - time_2:.2f}/actual{time_4 - time_3:.2f}/viz{time_6 - time_5:.2f}/pad{time_1 - time_0:.2f}"
+                    )
+                time_7 = time.time()
+                tstq.set_description(
+                    f"pad{time_1 - time_0:.2f}/infer{time_7 - time_1:.2f}:recon{time_3 - time_2:.2f}/actual{time_4 - time_3:.2f}/viz{time_6 - time_5:.2f}"  # pad0.00/infer2.46:recon0.00/actual0.00/viz2.46
+                )  # time for all in batch
+
+                # print("saved uvz comparison plot.", plot_dir)
 
 
 def main():